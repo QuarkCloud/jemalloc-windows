d73e29ac0250d1135120eeacad0829c2822ef7e7
romandion <romandion@163.com>
2018-06-13 18:24:09 +0800

    add unit project

--- include/jemalloc/internal/malloc_io.h
+++ include/jemalloc/internal/malloc_io.h
@@ -1,26 +1,8 @@
 
 #ifndef __JEMALLOC_INTERNAL_MALLOC_IO_H
 #define __JEMALLOC_INTERNAL_MALLOC_IO_H
-/**
-#ifdef _WIN32
-#  ifdef _WIN64
-#    define FMT64_PREFIX "ll"
-#    define FMTPTR_PREFIX "ll"
-#  else
-#    define FMT64_PREFIX "ll"
-#    define FMTPTR_PREFIX ""
-#  endif
-#  define FMTd32 "d"
-#  define FMTu32 "u"
-#  define FMTx32 "x"
-#  define FMTd64 FMT64_PREFIX "d"
-#  define FMTu64 FMT64_PREFIX "u"
-#  define FMTx64 FMT64_PREFIX "x"
-#  define FMTdPTR FMTPTR_PREFIX "d"
-#  define FMTuPTR FMTPTR_PREFIX "u"
-#  define FMTxPTR FMTPTR_PREFIX "x"
-#else
-*/
+
+
 #  include <inttypes.h>
 #  define FMTd32 PRId32
 #  define FMTu32 PRIu32
